c2acd64c05b3b5aa8e59448889856d646ebd93db
Craig Dykstra <craig@craigdykstra.com>
2016-06-03 22:44:06 -0400

    admin login and logout working login styled fileView styling started

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var express = require("express")
+var session = require("express-session")
+var bodyParser = require("body-parser")
 var fs = require("fs")
 var Handlebars = require("handlebars")
 var passport = require("passport")
 var LocalStrategy = require("passport-local").Strategy
 var appRoot = require("app-root-path")
+var Sha256 = require("./public/Sha256")
 
 // load up config
 var defaultConfig = require("./default.config")
@@ -23,25 +26,39 @@ for (var k in defaultConfig) {
 	}
 }
 
+var secret = "M^Secr3tIsB3tter"
+
 var users = {
-	[config.adminUser]: { _id: 0, admin: true, userName: "admin", password: config.adminPassword }
+	[config.adminUserName]: {
+		admin: true,
+		userName: config.adminUserName,
+		password: Sha256.hash(Sha256.hash(config.adminUserName + config.adminPassword) + secret)
+	}
 }
 
 // setup passport
 passport.use(new LocalStrategy(
-	(userName, password, done) => {
+	{
+		passReqToCallback: true
+	},
+	(req, userName, password, done) => {
 		var user = users[userName.toLowerCase()]
 		if (!user) {
-			done(null, false, { message: "invalid username" })
-		} else if (password !== user.password) {
-			done(null, false, { message: "invalid password" })
+			req.session.loginMessage = "invalid username"
+			return done(null, false)
+		} else if (Sha256.hash(password + secret) !== user.password) {
+			req.session.loginMessage = "invalid password"
+			return done(null, false)
 		}
-		done(null, user)
+		done(null, {
+			admin: user.admin,
+			userName: user.userName
+		})
 	}
 ))
 
 passport.serializeUser((user, done) => {
-	done(null, user._id);
+	done(null, user.userName);
 })
 
 passport.deserializeUser((id, done) => {
@@ -50,52 +67,95 @@ passport.deserializeUser((id, done) => {
 
 // set up templates
 var templates = {}
-fs.readdir(__dirname + "/views", (err, files) => {
-	files.forEach((file, i) => {
-		var templateName = file.split(".")[0]
-		fs.readFile(__dirname + "/views/"+file, "utf8", (err, data) => {
-			templates[templateName] = Handlebars.compile(data)
-			Handlebars.registerPartial(templateName, data)
+function loadTemplates(callback) {
+	fs.readdir(__dirname + "/views", (err, files) => {
+		var fileCount = 0
+		files.forEach((file, i) => {
+			var templateName = file.split(".")[0]
+			fs.readFile(__dirname + "/views/"+file, "utf8", (err, data) => {
+				templates[templateName] = Handlebars.compile(data)
+
+				Handlebars.unregisterPartial(templateName)
+				Handlebars.registerPartial(templateName, data)
+
+				fileCount++
+				if (fileCount == files.length && callback) {
+					callback()
+				}
+			})
 		})
 	})
-})
+}
 
 class ExpressDrive {
 	constructor(app, passedConfig) {
 		this.app = app
-		this.passportRedirectObj = {
-			successRedirect: config.path,
-			failureRedirect: config.path + '/login',
-			failureFlash: true
+		this.restrictedPaths = {
+			f: true
 		}
 
 		this.init()
 	}
 
 	init() {
+		this.app.use(bodyParser.urlencoded({ extended: false }))
+		this.app.use(bodyParser.json())
+		this.app.use(session({
+			secret,
+			resave: false,
+			saveUninitialized: true,
+			httpOnly: true,
+			secure: false
+		}));
 		this.app.use("/expressdrive", express.static(__dirname + "/public"));
-		this.app.use(passport.initialize());
-		this.app.use(passport.session());
+		this.app.use(passport.initialize())
+		this.app.use(passport.session())
 
-		this.app.get(config.path,
-			passport.authenticate("local", this.passportRedirectObj),
+		this.app.use("*",
+			(req, res, next) => {
+				if (req.originalUrl.substring(0,config.path.length) == config.path) {
+					loadTemplates(() => {
+						var urlComponents = req.originalUrl.substring(config.path.length).split("/")
+						if (!req.user && this.restrictedPaths[urlComponents[1]]) {
+							return res.redirect(config.path + "/login?oreq=" + req.originalUrl)
+						}
+						next()
+					})
+				}
+			}
+		)
+
+		this.app.get([config.path, config.path + "/f"],
 			(req, res) => {
-				res.send("<h1>Hello from ExpressDrive!</h1>")
+				if (!req.user) { return res.redirect(config.path + "/login") }
+				res.send(templates.main({ page: "fileView", path: config.path, user: req.user }))
 			}
 		)
 
 		this.app.get(config.path + "/login",
 			(req, res) => {
-				if (req.user) {
-					req.redirect(config.path)
-				} else {
-					res.send(templates.main({ page: "login" }))
-				}
+				if (req.user) { return res.redirect(config.path) }
+
+				var message = req.session.loginMessage
+				delete req.session.loginMessage
+				res.send(templates.main({ page: "login", path: config.path, message: message }))
 			}
 		)
 
 		this.app.post(config.path + "/login",
-			passport.authenticate('local', this.passportRedirectObj)
+			passport.authenticate("local", {
+				failureRedirect: config.path + "/login",
+				successRedirect: config.path
+			})
+		)
+
+		this.app.get(config.path + "/logout",
+			(req, res) => {
+				if (req.user) {
+					req.logout()
+				}
+				res.redirect(config.path + "/login")
+			}
 		)
 	}
 }
